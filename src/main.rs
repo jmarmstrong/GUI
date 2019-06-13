0579aedc59e10e96c87f2b4d1b04ff8bb351f1bc
TechResort <techresort@Unknown-68-f7-28-8a-a1-f2.home>
2019-06-13 18:16:36 +0100

    created gui

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,17 @@
 use gtk::prelude::*;
+use gtk::{Application, ApplicationWindow, Button, Window};
+const GLADE_UI:&str = include_str!("../main.glade");
 
 fn main() {
-    println!("Hello, world!");
+    if gtk::init().is_err(){
+        println!("GTK has failed me!");
+        return;
+    }
+    let builder = gtk::Builder::new_from_string(GLADE_UI);
+    let button:Button = builder.get_object("button").unwrap();
+    button.connect_clicked(|_|println!("Goodbye my Friend"));     
+    let window:Window = builder.get_object("window").unwrap();
+    window.show_all();
+    gtk::main();
 }
+
