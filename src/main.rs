28b3c52f43443a4e0f36cd94d11fe89ea15ecf88
TechResort <techresort@Unknown-68-f7-28-8a-a1-f2.home>
2019-06-27 17:02:06 +0100

    update

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,9 @@ fn main() {
     }
     let builder = gtk::Builder::new_from_string(GLADE_UI);
     let button:Button = builder.get_object("button").unwrap();
+    let buttona:Button = builder.get_object("press").unwrap();
+    let windowa:Window = builder.get_object("simple").unwrap();
+    buttona.connect_clicked(move|_|windowa.show_all());
     button.connect_clicked(|_|println!("Goodbye my Friend"));     
     let window:Window = builder.get_object("window").unwrap();
     window.show_all();
