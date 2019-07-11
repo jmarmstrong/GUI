c2cb7dcac34366dc259635c51d8e8d437eb6b3bb
TechResort <techresort@Unknown-68-f7-28-8a-a1-f2.home>
2019-07-11 18:24:26 +0100

    not working right now

--- src/main.rs
+++ src/main.rs
@@ -11,17 +11,38 @@ fn main() {
     let button: Button = builder.get_object("button").unwrap();
     let buttona: Button = builder.get_object("press").unwrap();
     let windowa: Window = builder.get_object("info1").unwrap();
-    let toggle: Button = builder.get_object("toggle").unwrap();
-    let info: Window = builder.get_object("info2").unwrap();
-    toggle.connect_clicked(move |_| info.show_all());
+    let toggle: Button = builder.get_object("togglea").unwrap();
+    let toggleb: Button = builder.get_object("toggleb").unwrap();
+    let windowb: Window = builder.get_object("info2").expect("it faild here");
+
+    {
+        let windowa = windowa.clone();
+        toggle.connect_clicked(move |_| {
+            windowb.show_all();
+            windowa.hide();
+        });
+    }
+
     {
         let windowa = windowa.clone();
         buttona.connect_clicked(move |_| windowa.clone().show_all());
     }
+
     windowa.connect_delete_event(|w, _| {
         w.hide();
         Inhibit(true)
-});
+    });
+
+    {
+        let windowb = windowb.clone();
+        toggleb.connect_clicked(move |_| windowb.clone().show_all());
+    }    
+
+    windowb.connect_delete_event(|w, _| {
+        w.hide();
+        Inhibit(true)
+    });
+
     button.connect_clicked(|_| println!("Goodbye my Friend"));
     let window: Window = builder.get_object("window").unwrap();
     window.show_all();
